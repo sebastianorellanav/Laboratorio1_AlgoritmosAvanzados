ff3300a8a0220ad4560d7261ef99c9bfbdca129f
sebastian orellana v <sebastian.orellana.v@usach.cl>
2019-10-11 17:00:58 -0300

    backtraking was fixed and push, pop and others was created

--- laboratorio1.c
+++ laboratorio1.c
@@ -5,124 +5,161 @@ typedef struct nodo{
 	int x;
 	int y;
 	int id;
-	struct nodo* siguiente;
+	int previousId;
+	struct node* next;
 }nodo;
 
-nodo* crearNodo(int posX, int posY, int numId){
-	nodo *new = NULL;
-   	new = (nodo*)malloc(sizeof(nodo));
+node* createNode(int posX, int posY, int numId, int numPreviousId){
+	node *new = NULL;
+   	new = (node*)malloc(sizeof(node));
    	new -> x = posX;
    	new -> y = posY;
    	new -> id = numId;
-   	new -> siguiente = NULL;
+   	new -> previousId = numPreviousId;
+   	new -> next = NULL;
    	return new;
 }
 
-void insertarAlInicio(nodo **lista, int posX, int posY){
-	if(*lista == NULL){
-		*lista = crearNodo(posX, posY, 0);
+//function to add an element to stack
+//inputs = stack (by reference), position X, position Y 
+void push(node **stack, int posY, int posX){
+	if(*stack == NULL){
+		*stack = crearNodo(posX, posY, 0, -1);
 	}
 	else{
-		nodo *aux = *lista;
-		*lista = crearNodo(posX, posY, (aux->id)++);
-		*lista -> siguiente = aux;
+		node *aux = *satck;
+		*satck = createNode(posX, posY, (aux->id)++, aux->id);
+		*stack -> next = aux;
 	}
 }
 
-void readFile(char *nombreArchivo, nodo **lista, int *ancho, int *alto){
-	//Definir variables
-	int nLineasSiguientes = 0, aux1 = 0, aux2 = 0;
-	FILE *archivo = NULL;
+void pop(node **stack){
+	node *aux = *stack->next;
+	free(*stack);
+	*stack = aux;
+}
 
-	//Abrir archivo
-  	archivo = fopen(nombreArchivo, "r");
+//function to read the input file
+//inputs = name, list (by reference), width (by reference), length (by reference)
+void readFile(char *fileName, nodo **list, int *widthMatrix, int *lengthMatrix){
+	//define variables
+	int nextLines = 0, aux1 = 0, aux2 = 0;
+	FILE *archive = NULL;
 
-  	//verificar si el archivo se abrió correctamente
-  	if(archivo == NULL){    
-    	printf("  Error: No se ha podido abrir el archivo %s", nombreArchivo);
+	// open file
+  	archive = fopen(fileName, "r");
+
+  	//check if file was opened correctly
+  	if(archive == NULL){    
+    	printf("  Error: No se ha podido abrir el archivo %s", fileName);
     	exit(1);
   	}
 
-  	//guardar ancho y alto de la matriz
-  	fscanf(archivo, "%d", ancho);
-  	fscanf(archivo, "%d", alto);
+  	// read and save width and long of matrix
+  	fscanf(archive, "%d", widthMatrix);
+  	fscanf(archive, "%d", lengthMatrix);
 
-  	//Guardar el numero de lineas siguientes
-  	fscanf(archivo, "%d", &nLineasSiguientes);
+  	// read and save the number of next lines
+  	fscanf(archive, "%d", &nextLines);
 
-  	//Guardar lineas Siguientes
-  	for (int i = 0; i < nLineasSiguientes; ++i){
-  		fscanf(archivo, "%d", &aux1);
-  		fscanf(archivo, "%d", &aux2);
-  		insertarAlInicio(lista, aux1, aux2);
+  	// read and save the content of next lines
+  	for (int i = 0; i < nextLines; ++i){
+  		fscanf(archive, "%d", &aux1);
+  		fscanf(archive, "%d", &aux2);
+  		push(list, aux1, aux2);
   	}
 
 
 }
 
-void colocarSucursalesFijas(int **matriz, nodo *lista){
-	nodo *aux = lista;
+//place the locations that cant change in the matrix
+//inputs = int matrix, list
+void placeFixedLocations(int **matrix, nodo *list){
+	node *aux = list;
 	while(aux != NULL){
-		matriz[aux->y][aux->x] = 2;
+		matrix[aux->y][aux->x] = 2;
 	}
 }
 
-//backtracking algorithm
-void backtracking(int n, int** matrix){
-	int columna = -1;
-	int fila = 0;
+//backtracking algorithm: search the max number of locations possibles and return them
+//inputs = int matrix, width of Matrix, length of Matrix
+void backtracking(int** matrix, int width, int length){
+	//define variables
+	int column = -1;
+	int maxLocations = 0;
+	int row = 0;
 	bool aux = TRUE;
-	nodo *lista = NULL;
-	nodo *maximo = NULL;
+	nodo *stack = NULL;
 
-	//mientras se puedan colocar filas
-	while (file >= 0){
+	//define maximum number of locations
+	if (width > length)
+		maxLocations = length;
+	else
+		maxLocations = width;
+
+	//if matrix == 2x2 matrix
+	if ((width == length) && (width == 2)){
+		// exist only one location possible
+		push(&stack, 0, 0);
+		row = -1;
+	}
+
+	//if matrix == 3x3 matrix
+	if ((width == length) && (width == 3)){
+		//exist only two locations possible
+		push(&stack, 0, 0);
+		push(&stack, 1, 2);
+		row = -1;
+	}
+
+	// while current row is valid
+	while (row >= 0){
+		//do
 		do
-		{
+		{	
+			//move on to next column
 			column++;
+			//check if a branchOffice can be built in current location
 			aux = checkPosition(matrix, row, column);
-		} while ((column < n) && not(aux))
+  		  //while current column is valid and current location isn't avaiable to build
+		} while ((column < width) && not(aux))
 		
-		//Si quedé dentro de la matriz
-		if (column < n){
-			//guardar numero en matriz
-			//guardar posicion en lista
+		// if current column is valid
+		if (column < width){
+			// save position in matrix
+			matrix[row][column] = 1;
 
-			insert(&list, row, column);
-			//si estoy en ultima fila (logre colocar el maximo)
-			if (row == n-1){
-				//return list
+			// save location in stack
+			push(&stack, row, column);
+			
+			// if current row == last row  or  length of stack == max number of locations
+			if (row == length-1 || lengthStack(stack) == maxLocations){
+				// finish while
+				row = -1;
 			}
-			//si no estoy en la ultima fila
+			// if current row != last row
 			else{
-				fila++;
+				//move on to the next row
+				row++;
+				//go back before the first column
 				column = -1;
 			}
 		}
-		//Si no quede dentro de la matriz
+		// if current column is not valid 
 		else{
-			if(maximo == NULL || lista->tamaño >= maximo->tamaño){
-				aux = maximo;
-				borrarDesdeMaximo(&lista, aux);
-				maximo = lista;
-			}
-			else{
-
-			}
+			// delete last location in stack
+			pop(&stack);
+			//go back to previous row
 			k--;
 		}
 	}
+	printStack(stack);
 }
 
-for (int i = 0; i < alto; ++i){
-	for (int j = 0; j < ancho; ++j){
-		if(posicion no es valida){
-			j++;
-		}
-		else{
-			//guardar numero en matriz
-			//guardar posicion en lista
-		}
+void printStack(node *stack){
+	while(stack != NULL){
+		printf("x = %d || y = %d || id = %d\n", stack->x, stack->y, stack-> id);
+		stack = stack->next;
 	}
 }
 
@@ -146,29 +183,32 @@ mientras k > 0
 
 
 void main(){
-	//crear variables
-	int ancho = 0;
-	int alto = 0;
-	char *nombre;
-	nodo *lista = NULL;
+	//define variables
+	int widthMatrix = 0;
+	int lengthMatrix = 0;
+	char *fileName = "entrada.in";
+	node *list = NULL;
 	
-	//leer archivo
-	readFile(nombre, &lista,  &m, &n);
+	//read input file
+	readFile(fileName, &list,  &widthMatrix, &lengthMatrix);
 
-	//crear matriz
-	matrix = (int**)malloc(alto*sizeof(int*));
-	for (int i = 0 ; i < alto ; i++)
-    	matrix[i] = (int *) malloc (ancho*sizeof(int));
+	//create matrix
+	matrix = (int**)malloc(lengthMatrix*sizeof(int*));
+	for (int i = 0 ; i < lengthMatrix ; i++)
+    	matrix[i] = (int *) malloc (widthMatrix*sizeof(int));
 
-    //inicializar la matriz con ceros
-    for (int i = 0; i < alto; ++i)    {
-    	for (int j = 0; i < ancho; ++j){
+    //inicializate matrix with 0s
+    for (int i = 0; i < lengthMatrix; ++i)    {
+    	for (int j = 0; i < widthMatrix; ++j){
     		matrix[i][j] = 0;
     	}
     }
 
-    colocarSucursalesFijas(matriz, lista);
-    liberarLista(&lista);
+    //placed the location that cant change 
+    placeFixedLocations(matriz, lista);
 
+    //search the solution
+    backtracking(matrix, widthMatrix, lengthMatrix);
 
+    return 0;
 	}
